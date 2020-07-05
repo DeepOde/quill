d2538066abd23046f5e22105ca51aac192df92fb
DeepOde <ashdeepode@gmail.com>
2020-07-05 12:43:32 +0530

    Prototype ready

--- index.py
+++ index.py
@@ -1,5 +1,26 @@
 from flask import Flask, render_template, request, jsonify
+import random
 app = Flask(__name__)
+books = []
+books.append('teach/hp1.txt')
+def get_wordlist(books, lower=False):
+    '''Return complete book in form of list, each entry containing one word
+    '''
+    wordlist = []
+    print("Book reading initiated...")
+    for b in books:
+        with open(b, errors='ignore') as f:
+            raw_f = f.read()
+            if lower:
+                raw_f = raw_f.lower()
+    
+            print("Writing down wordlist for "+b+"....")
+            for word in raw_f.split():
+                wordlist.append(word)
+            print("Wordlist prepared for "+b+"!")
+
+    return wordlist
+wordlist = get_wordlist(books)
 
 @app.route('/')
 def hello_world():
@@ -7,10 +28,29 @@ def hello_world():
 
 @app.route('/res', methods=['POST'])
 def res():
-    s = 'abcdefghijkl'
-    l = list(s)
-    req = request.get_json()
-    print(req)
-    res = jsonify(l[req]), 200
+    order = 2 #In future, let user set this
 
+    worddict = {} #Each key is string representing word, value will be list of words appearing after string in key
+
+    for i, word in enumerate(wordlist):
+        if (i+order) < len(wordlist):
+            key = ' '.join(wordlist[i:i+order])
+            append_to_value = ' '.join(wordlist[i+order:i+order+order])
+            worddict[key] = worddict.get(key, []) + [append_to_value]
+    
+    req = ''
+    req = request.get_json()
+    #print(type(req))
+    
+    if req == '':
+        pick_key = random.choice(list(worddict))
+    else:
+	    pick_key = req
+    #print(pick_key)
+    choices = []
+    for _ in range(12):
+        choices.append(random.choice(worddict[pick_key]))
+    #print(choices)
+    res = jsonify(choices), 200
     return res
+    
